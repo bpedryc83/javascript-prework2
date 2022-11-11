d8ccff71997ce12d116ef5f2a7232855847960cb
Bartlomiej Pedryc <b.pedryc@poczta.fm>
2022-11-11 13:13:20 +0100

    Replace variables to the const type, exchange named functions for anonymous functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+{
 let playerResult = 0;
 let computerResult = 0;
 
-function playGame(playerInput){
+const playGame = function(playerInput){
     clearMessages();    
     
-    function getMoveName(argMoveId){
+    const getMoveName = function(argMoveId){
         console.log('Wykonano funkcję getMoveName');
         if(argMoveId == 1){
         return 'kamień';
@@ -19,9 +20,9 @@ function playGame(playerInput){
         printMessage('Nie znam ruchu o id ' + argMoveId + '.');
         return 'nieznany ruch';
         }
-    }
+    };
 
-    function displayResult (argComputerMove, argPlayerMove){
+    const displayResult = function(argComputerMove, argPlayerMove){
         if((argComputerMove == 'kamień' && argPlayerMove == 'papier') 
         || (argComputerMove == 'papier' && argPlayerMove == 'nożyce')
         || (argComputerMove == 'nożyce' && argPlayerMove == "kamień")){
@@ -35,16 +36,15 @@ function playGame(playerInput){
             printMessage('Przegrałeś');
             computerResult = computerResult + 1;
         }
-    }
+    };
 
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    let computerMove = getMoveName(randomNumber);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    const computerMove = getMoveName(randomNumber);
 
     console.log('Wylosowana liczba to: ' + randomNumber);
     printMessage('<span style="color:red; font-weight:bold;">Ruch komputera to: ' + computerMove + '</span>');
 
-    /* let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'); */
-    let playerMove = getMoveName(playerInput);
+    const playerMove = getMoveName(playerInput);
     console.log('Gracz wpisał: ' + playerInput);
     printMessage('<span style="color:green; font-weight:bold;">Twój ruch to: ' + playerMove + '</span>');
 
@@ -52,7 +52,7 @@ function playGame(playerInput){
     console.log('argMoveID w funkcji getMoveName przyjęło dla Ciebie wartość: ',playerInput);
 
     printEndResult(playerResult + ' - ' + computerResult);
-}
+};
 
 document.getElementById('play-rock').addEventListener('click', function(){
     playGame(1);
@@ -63,3 +63,4 @@ document.getElementById('play-paper').addEventListener('click', function(){
   document.getElementById('play-scissors').addEventListener('click', function(){
     playGame(3);
   });
+}
